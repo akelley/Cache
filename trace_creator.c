99d1052d4c36b59b6dfc01d7a4fb178846c1e566
Andrew Kelley <iodine73@gmail.com>
2017-02-24 12:25:12 -0500

    Add files via upload

---
+++ trace_creator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int generateHexNumber(){
+	int number = rand() & 0xff;
+	number |= (rand() & 0xff) << 8;
+	number |= (rand() & 0xff) << 16;
+	number |= (rand() & 0xff) << 24;
+	return number;
+}
+
+int main(){
+	int traceType;
+	int lines = 0;
+	char var;
+
+	FILE *traceFile;
+	traceFile = fopen("trace2.txt", "w");
+	
+	printf("Press 0 for a random trace; Press 1 for a sequential trace: ");
+	scanf("%d", &traceType);
+
+	if(traceType == 0){
+		while(lines != 100){
+			int address = generateHexNumber();
+			fprintf(traceFile, "0 %x\n", address);
+			lines++;
+		}
+	}
+
+	else{
+		int address = 0;
+
+		while(lines != 100){	
+			fprintf(traceFile, "0 %x\n", address);
+			address += 4;
+			lines++;
+		}
+	}
+
+	printf("Trace file population complete.\n");
+	printf("Press any key to exit: ");
+	var = getch();
+
+
+	/*if(var == 'X' || var == 'x'){
+		printf("Thank you!\n");
+	}
+
+	else{
+		printf("That's not an 'X'! We're all doomed!\n");
+	}*/
+
+	fclose(traceFile);
+	exit(0);
+}
